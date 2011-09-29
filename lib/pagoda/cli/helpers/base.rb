846055ab650ead7fb49bfe5bd5c87b8c595ebb7c
Lyon <lyon@delorum.com>
2011-09-29 14:29:49 -0600

    commit my changes

--- lib/pagoda/cli/helpers/base.rb
+++ lib/pagoda/cli/helpers/base.rb
@@ -7,16 +7,74 @@ module Pagoda
     class Base
       GIT_REGEX = /^((git@github.com:)|(.*:)|((http|https):\/\/.*github.com\/)|(git:\/\/github.com\/))(.*)\/(.*).git$/i
       include Pagoda::Helpers
+
+      class << self
+        include Pagoda::Helpers
+        def ask_for_credentials
+          username = ask "Username: "
+          display "Password: ", false
+          password = running_on_windows? ? ask_for_password_on_windows : ask_for_password
+          [username, password] # return
+        end
+
+        def ask_for_password
+          echo_off
+          password = ask
+          puts
+          echo_on
+          return password
+        end
+
+        def ask_for_password_on_windows
+          require "Win32API"
+          char = nil
+          password = ''
+          
+          while char = Win32API.new("crtdll", "_getch", [ ], "L").Call do
+            break if char == 10 || char == 13 # received carriage return or newline
+            if char == 127 || char == 8 # backspace and delete
+              password.slice!(-1, 1)
+            else
+              # windows might throw a -1 at us so make sure to handle RangeError
+              (password << char.chr) rescue RangeError
+            end
+          end
+          return password
+        end
+
+        def echo_off
+          silently(system "stty -echo")
+        rescue
+        end
+
+        def echo_on
+          silently(system "stty echo")
+        rescue
+          
+      end
+
       
       attr_reader :client
+      attr_reader :globals
+      attr_reader :options
       attr_accessor :args
 
-      def initialize(args)
+      def initialize(globals, options, args)
+        @globals = globals
+        @options = options
         @args = args
       end
 
+      def user
+        globals[:username]
+      end
+
+      def password
+        globals[:password]
+      end
+
       def client
-        @client ||= Pagoda::Client.new(Pagoda::Auth.user, Pagoda::Auth.password)
+        @client ||= Pagoda::Client.new(user, password)
       end
       
       def shell(cmd)
@@ -24,11 +82,11 @@ module Pagoda
       end
       
       def remote
-        option_value("-r", "--remote") || "pagoda"
+        options[:remote]
       end
 
       def app(soft_fail=false)
-        if app = option_value("-a", "--app")
+        if app = globals[:app] || options[:app]
           app
         elsif app = extract_app_from_git_config
           app
@@ -38,7 +96,7 @@ module Pagoda
           if soft_fail
             false
           else
-            error "Unable to find the app. please specify using -a or --app"
+            raise "Unable to find the app. please specify using -a or --app"
           end
         end
       end
@@ -73,41 +131,23 @@ module Pagoda
       end
 
       def branch
-        option_value("-b", "--branch") || find_branch
+        options[:branch] || find_branch
       end
       
       def commit
-        option_value("-c", "--commit") || find_commit
+        options[:commit] || find_commit
       end
       
       def find_branch
         git "name-rev --refs=$(git symbolic-ref HEAD) --name-only HEAD"
-        # begin
-        #   line = File.new("#{locate_app_root}/.git/HEAD").gets
-        #   line.strip.split(' ').last.split("/").last
-        # rescue
-        #   nil
-        # end
       end
       
       def find_commit
         git "rev-parse --verify HEAD"
-        # begin
-        #   File.new("#{locate_app_root}/.git/refs/heads/#{branch}").gets.strip
-        # rescue 
-        #   nil
-        # end
       end
       
       def extract_git_clone_url(soft=false)
         git("config remote.pagoda.url")
-        # begin
-        #   url = IniParse.parse( File.read("#{locate_app_root}/.git/config") )['remote "origin"']["url"]
-        #   raise unless url.match(GIT_REGEX)
-        #   url
-        # rescue Exception => e
-        #   return false
-        # end
       end
       
       def locate_app_root(dir=Dir.pwd)
@@ -128,24 +168,6 @@ module Pagoda
           end
         end
       end
-
-      # def extract_option(options, default=true)
-      #   values = options.is_a?(Array) ? options : [options]
-      #   return unless opt_index = args.select { |a| values.include? a }.first
-      #   opt_position = args.index(opt_index) + 1
-      #   if args.size > opt_position && opt_value = args[opt_position]
-      #     if opt_value.include?('--')
-      #       opt_value = nil
-      #     else
-      #       args.delete_at(opt_position)
-      #     end
-      #   end
-      #   opt_value ||= default
-      #   args.delete(opt_index)
-      #   block_given? ? yield(opt_value) : opt_value
-      # end
-
-
         
     end
   end
